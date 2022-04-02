685d9a677bff8512bb73c51bbc0108731076bb2c
Basharabulaban <ba.ahmad@gmail.com>
2022-04-02 23:21:27 +0300

    person db

--- src/main/java/com/densin/rws/jdbctojpa/jpa/PersonJpaRepository.java
+++ src/main/java/com/densin/rws/jdbctojpa/jpa/PersonJpaRepository.java
@@ -1,15 +1,23 @@
 package com.densin.rws.jdbctojpa.jpa;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+//import com.densin.rws.jdbctojpa.PersonJdbcDao.PersonRowMapper;
 import com.densin.rws.jdbctojpa.entity.Person;
 
 //Repository
-//transaction
+//transactional
+
 @Repository
 @Transactional
 public class PersonJpaRepository {
@@ -17,13 +25,43 @@ public class PersonJpaRepository {
 	@PersistenceContext
 	
 	EntityManager entityManager;
+	
+	
+	
+	class PersonRowMapper implements RowMapper<Person>{
+
+		@Override
+		public Person mapRow(ResultSet rs, int rowNum) throws SQLException {
+			Person person= new Person();
+			
+			person.setId(rs.getInt("id"));	
+			person.setName(rs.getString("name"));	
+			person.setLocation(rs.getString("location"));	
+			person.setBirthDate(rs.getTimestamp("birth_date"));	
+			return person;
+		}
+		
+	}
+	
+	
 	public Person update(Person person) {
 		return entityManager.merge(person);
 	}
 	public Person findbyId(int id) {
 		return entityManager.find(Person.class, id);
 	}
-
+	public void Deletebyid(int id) {
+		Person person = findbyId(id);
+		 entityManager.remove(person);
+	}
 	
-
+	public List<Person> findAll(){
+		TypedQuery<Person> namedQuery= entityManager.createNamedQuery("find_all_persons",Person.class);
+		return namedQuery.getResultList();
+		
+		
+	
+			
+	//"select * from person",new PersonRowMapper()
+		}
 }
