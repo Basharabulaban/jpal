9b0c6cbbdee86c748797dbded389d82311ed7d20
Basharabulaban <ba.ahmad@gmail.com>
2022-04-04 00:40:10 +0300

    start unit test  junit5

---
+++ src/test/java/com/densin/rws/jdbctojpa/CourseRepositoryTest.java
@@ -0,0 +1,15 @@
+package com.densin.rws.jdbctojpa;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest(classes=JpaApplication.class)
+class CourseRepositoryTest {
+//s
+	@Test
+	void contextLoads() {
+	}
+
+}
