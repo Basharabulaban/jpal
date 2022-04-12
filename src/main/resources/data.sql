0a473bb25b137a2c4478fcc54e1be528a551aa77
Basharabulaban <ba.ahmad@gmail.com>
2022-04-13 00:27:51 +0300

    insert some new records

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -30,3 +30,29 @@ insert into course(id,name,create_date,last_update_date)
 values(10002L,'Spring in 100 steps',sysdate(), sysdate());
 insert into course(id,name,create_date,last_update_date) 
 values(10003L,'Java in 10 steps',sysdate(), sysdate());
+
+
+
+insert into student(id,name)
+values(20001,'bashar');
+insert into student(id,name)
+values(20002,'Yaman');
+insert into student(id,name)
+values(20003,'Ahmad');
+
+insert into passport(id,number)
+values(40001,'B123456');
+insert into passport(id,number)
+values(40002,'Y123456');
+insert into passport(id,number)
+values(40003,'A123456');
+
+insert into Review(id,rating,description)
+values(50001,'5','Great Course');
+
+insert into Review(id,rating,description)
+values(50002,'4','Wonderfull Course');
+
+insert into Review(id,rating,description)
+values(50003,'4','Awesome Course');
+
