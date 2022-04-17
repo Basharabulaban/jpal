b0add23004da1e64692ad455c646dc7ab88bbfe6
Basharabulaban <ba.ahmad@gmail.com>
2022-04-18 00:11:49 +0300

    maintain new queries

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -31,21 +31,24 @@ values(10002L,'Spring in 100 steps',sysdate(), sysdate());
 insert into course(id,name,create_date,last_update_date) 
 values(10003L,'Java in 10 steps',sysdate(), sysdate());
 
-
-
-insert into student(id,name,passport_id)
-values(20001,'bashar',40001);
-insert into student(id,name,passport_id)
-values(20002,'Yaman',40002);
-insert into student(id,name,passport_id)
-values(20003,'Ahmad',40003);
-
 insert into passport(id,number)
 values(40001,'B123456');
 insert into passport(id,number)
 values(40002,'Y123456');
 insert into passport(id,number)
 values(40003,'A123456');
+// if you put the table  passport after student , an issues will happen
+// select * from student , passport  where student.passport_id = passport.id
+// Passport_ID is foriegn ke , which is id of another table or the primary key of passport key , and use it forign key in student table
+
+insert into student(id,name,PASSPORT_ID)
+values(20001,'bashar',40001);
+insert into student(id,name,PASSPORT_ID)
+values(20002,'Yaman',40002);
+insert into student(id,name,PASSPORT_ID)
+values(20003,'Ahmad',40003);
+
+
 
 insert into Review(id,rating,description)
 values(50001,'5','Great Course');
