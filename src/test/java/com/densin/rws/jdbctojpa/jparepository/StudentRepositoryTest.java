dd1b37bdc9f38839b8861996816ce07a6fdb3f78
Basharabulaban <ba.ahmad@gmail.com>
2022-04-20 22:49:45 +0300

    OneToOne mapping

---
+++ src/test/java/com/densin/rws/jdbctojpa/jparepository/StudentRepositoryTest.java
@@ -0,0 +1,101 @@
+package com.densin.rws.jdbctojpa.jparepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import javax.persistence.EntityManager;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import com.densin.rws.jdbctojpa.JpaApplication;
+import com.densin.rws.jdbctojpa.entity.Student;
+
+import net.bytebuddy.asm.Advice.This;
+
+@SpringBootTest(classes = JpaApplication.class)
+class StudentRepositoryTest {
+	private Logger logger = LoggerFactory.getLogger(This.class);
+	@Autowired
+	StudentRepository repository;
+
+@Autowired
+EntityManager em;
+	@Test
+	void retreiveStudentandPasportdetails() {
+		
+
+	}
+	
+	
+	@Test
+	void findById_basic() {
+		Student student = repository.findById(10001L);
+
+		assertEquals("JPA in 50 steps", student.getName());
+//		logger.info("test is running ->",student);
+
+	}
+
+	
+	@Test
+	 @DirtiesContext   // in order to return back data as it were 
+	void deleteById_basic() {
+		repository.deleteById(10002L);
+		assertNull(repository.findById(10002L));
+
+//		logger.info("test is running ->",student);
+
+	}
+	
+	@Test
+	 @DirtiesContext   // in order to return back data as it were 
+	void saveByEntity_basic() {
+		Student student = new Student("student name 2");
+		repository.save(student);
+		
+	//	assertNull(repository.findById(10002L));
+		assertEquals("student name 2", student.getName());
+//		logger.info("test is running ->",student);
+
+	}
+	
+	@Test
+	 @DirtiesContext   // in order to return back data as it were 
+	void UpdateByEntity_basic() {
+		
+		Student student = repository.findById(10002L);
+		assertEquals("Spring in 150 steps", student.getName());
+		
+		student.setName("Spring in 50 steps");
+		repository.save(student);
+		
+		Student student1 = repository.findById(10002L);
+		assertEquals("Spring in 50 steps", student1.getName());
+
+
+	}
+	
+	@Test
+	 @DirtiesContext   // in order to return back data as it were 
+	void playWithEnityManager() {
+		
+		repository.playWithEntityManager();
+	
+
+	}
+	
+	@Test
+	 @DirtiesContext   // in order to return back data as it were 
+	void playWithname_setnull() {
+		
+		repository.playWithname_setnull();
+	
+
+	}
+	
+}
