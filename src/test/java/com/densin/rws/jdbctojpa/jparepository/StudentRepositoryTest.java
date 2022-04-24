2f1d9ea5fd37ef4114eb3da478c6c8a48c56181b
Basharabulaban <ba.ahmad@gmail.com>
2022-04-24 22:24:50 +0300

    sometest @ transactional

--- src/test/java/com/densin/rws/jdbctojpa/jparepository/StudentRepositoryTest.java
+++ src/test/java/com/densin/rws/jdbctojpa/jparepository/StudentRepositoryTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
 import com.densin.rws.jdbctojpa.JpaApplication;
+import com.densin.rws.jdbctojpa.entity.Passport;
 import com.densin.rws.jdbctojpa.entity.Student;
 
 import net.bytebuddy.asm.Advice.This;
@@ -38,75 +39,86 @@ EntityManager em;
 	void sometest() {
 	// Database operation 1 - retireive student
 		Student student =em.find(Student.class, 20001L) ;
+		
+		logger.info("student ->",student);
 		// Database operation 1 - retireive passpoert
+	Passport passport =	student.getPassport();
+	logger.info("passport ->",passport);
+	
 		// Database operation 1 - update passport 
+	passport.setNumber("E123456");
+	
+	logger.info("passport.setNumber(\"E123456\") ->",passport);
 		// Database operation 1 - update student 
+	student.setName("Yaman - update");
+	logger.info("student ->",student);
+	
 	}
 	
-	@Test
-	void findById_basic() {
-		Student student = repository.findById(10001L);
-
-		assertEquals("JPA in 50 steps", student.getName());
-//		logger.info("test is running ->",student);
-
-	}
-
-	
-	@Test
-	 @DirtiesContext   // in order to return back data as it were 
-	void deleteById_basic() {
-		repository.deleteById(10002L);
-		assertNull(repository.findById(10002L));
-
-//		logger.info("test is running ->",student);
-
-	}
-	
-	@Test
-	 @DirtiesContext   // in order to return back data as it were 
-	void saveByEntity_basic() {
-		Student student = new Student("student name 2");
-		repository.save(student);
-		
-	//	assertNull(repository.findById(10002L));
-		assertEquals("student name 2", student.getName());
-//		logger.info("test is running ->",student);
-
-	}
-	
-	@Test
-	 @DirtiesContext   // in order to return back data as it were 
-	void UpdateByEntity_basic() {
-		
-		Student student = repository.findById(10002L);
-		assertEquals("Spring in 150 steps", student.getName());
-		
-		student.setName("Spring in 50 steps");
-		repository.save(student);
-		
-		Student student1 = repository.findById(10002L);
-		assertEquals("Spring in 50 steps", student1.getName());
-
-
-	}
-	
-	@Test
-	 @DirtiesContext   // in order to return back data as it were 
-	void playWithEnityManager() {
-		
-		repository.playWithEntityManager();
-	
-
-	}
-	
-	@Test
-	 @DirtiesContext   // in order to return back data as it were 
-	void playWithname_setnull() {
-		
-		repository.playWithname_setnull();
-	
-
-	}
+//	@Test
+//	void findById_basic() {
+//		Student student = repository.findById(10001L);
+//
+//		assertEquals("JPA in 50 steps", student.getName());
+////		logger.info("test is running ->",student);
+//
+//	}
+//
+//	
+//	@Test
+//	 @DirtiesContext   // in order to return back data as it were 
+//	void deleteById_basic() {
+//		repository.deleteById(10002L);
+//		assertNull(repository.findById(10002L));
+//
+////		logger.info("test is running ->",student);
+//
+//	}
+//	
+//	@Test
+//	 @DirtiesContext   // in order to return back data as it were 
+//	void saveByEntity_basic() {
+//		Student student = new Student("student name 2");
+//		repository.save(student);
+//		
+//	//	assertNull(repository.findById(10002L));
+//		assertEquals("student name 2", student.getName());
+////		logger.info("test is running ->",student);
+//
+//	}
+//	
+//	@Test
+//	 @DirtiesContext   // in order to return back data as it were 
+//	void UpdateByEntity_basic() {
+//		
+//		Student student = repository.findById(10002L);
+//		assertEquals("Spring in 150 steps", student.getName());
+//		
+//		student.setName("Spring in 50 steps");
+//		repository.save(student);
+//		
+//		Student student1 = repository.findById(10002L);
+//		assertEquals("Spring in 50 steps", student1.getName());
+//
+//
+//	}
+//	
+//	@Test
+//	 @DirtiesContext   // in order to return back data as it were 
+//	void playWithEnityManager() {
+//		
+//		repository.playWithEntityManager();
+//	
+//
+//	}
+//	
+//	@Test
+//	 @DirtiesContext   // in order to return back data as it were 
+//	void playWithname_setnull() {
+//		
+//		repository.playWithname_setnull();
+//	
+//
+//	}
 	
 }
